87a757c1426690e2b19c53c1eba22d5c6b231426
Harry Kalogirou <harkal@gmail.com>
2018-01-24 00:37:39 +0200

    remove json clutter

--- web3_dao/web3_dao.go
+++ web3_dao/web3_dao.go
@@ -1,59 +1,15 @@
 package web3_dao
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"math/big"
-	"net/http"
-	"sync"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/rpc"
 )
-
-type ResponseBase struct {
-	JSONRPC string          `json:"jsonrpc"`
-	ID      int64           `json:"id"`
-	Error   *ObjectError    `json:"error,omitempty"`
-	Result  json.RawMessage `json:"result,omitempty"`
-}
-
-type ObjectError struct {
-	Code    int64       `json:"code"`
-	Message string      `json:"message"`
-	Data    interface{} `json:"data"`
-}
-
-func (e *ObjectError) Error() string {
-	return e.Message
-
-	// var jsonrpc2ErrorMessages = map[int64]string{
-	// 	-32700: "Parse error",
-	// 	-32600: "Invalid Request",
-	// 	-32601: "Method not found",
-	// 	-32602: "Invalid params",
-	// 	-32603: "Internal error",
-	// 	-32000: "Server error",
-	// }
-	// fmt.Sprintf("%d (%s) %s\n%v", e.Code, jsonrpc2ErrorMessages[e.Code], e.Message, e.Data)
-}
-
-type Client struct {
-	url        string
-	httpClient *http.Client
-	id         int64
-	idLock     sync.Mutex
-}
-
-type Request struct {
-	JSONRPC string        `json:"jsonrpc"`
-	Method  string        `json:"method"`
-	ID      int64         `json:"id"`
-	Params  []interface{} `json:"params"`
-}
 
 var cli *rpc.Client
 
