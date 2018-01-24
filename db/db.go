3842082209f739ae8ae1be5f46daa158f7506ee8
Pantelis Giazitsis <giazitsispantelis@gmail.com>
2018-01-24 20:30:15 +0200

    insert block in db

--- db/db.go
+++ db/db.go
@@ -44,3 +44,56 @@ func NewClient() (*DBClient, error) {
 
 	return &DBClient{tdb}, nil
 }
+
+func (cli *DBClient) InsertBlocks(blocks []*models.Block) error {
+	stmt := `INSERT INTO blocks (
+		number, 
+		timestamp, 
+		hash, 
+		parent_hash, 
+		state_root, 
+		transactions_root, 
+		receipts_root, 
+		size, 
+		gas_used,
+		gas_limit)
+		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`
+	
+	if blocks==nil || len(blocks) == 0 {
+		return nil
+	}
+
+	bl := blocks[0]
+
+	log.Println(		
+		bl.Number, 
+		bl.TimeStamp, 
+		bl.Hash.String(), 
+		bl.ParentHash.String(), 
+		bl.StateRoot.String(), 
+		bl.TransactionsRoot.String(),
+		bl.ReceiptsRoot.String(),
+		bl.Size,
+		bl.GasUsed,
+		bl.GasLimit)
+
+	_, err := cli.db.Exec(stmt,
+		bl.Number, 
+		bl.TimeStamp, 
+		bl.Hash.String(), 
+		bl.ParentHash.String(), 
+		bl.StateRoot.String(), 
+		bl.TransactionsRoot.String(),
+		bl.ReceiptsRoot.String(),
+		bl.Size,
+		bl.GasUsed,
+		bl.GasLimit,
+	 )
+
+	 if err != nil {
+		 log.Println(err.Error())
+		 return err
+	 }
+
+	 return nil
+}
