34c228e2183228b1194ac833e2cdbb58fd97df11
Harry Kalogirou <harkal@gmail.com>
2018-01-25 10:11:16 +0200

    sanitization and optimizations

--- ipc/ipc.go
+++ ipc/ipc.go
@@ -1,10 +1,10 @@
 package ipc
 
 import (
-	"github.com/ethereum/go-ethereum/common"
 	"ebakus_server/models"
-	"math/big"
 	"log"
+
+	"github.com/ethereum/go-ethereum/common"
 
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/rpc"
@@ -26,21 +26,21 @@ func NewIPCInterface(endpoint string) (*IPCInterface, error) {
 //
 // Get the top block number
 //
-func (ipc *IPCInterface) GetBlockNumber() (*big.Int, error) {
+func (ipc *IPCInterface) GetBlockNumber() (uint64, error) {
 	var v hexutil.Big
 
 	err := ipc.cli.Call(&v, "eth_blockNumber")
 	if err != nil {
-		return nil, err
+		return 0, err
 	}
 
-	return v.ToInt(), nil
+	return v.ToInt().Uint64(), nil
 }
 
-func (ipc *IPCInterface) GetBlock(number *big.Int) (*models.Block, error) {
+func (ipc *IPCInterface) GetBlock(number uint64) (*models.Block, error) {
 	var block models.Block
 
-	err := ipc.cli.Call(&block, "eth_getBlockByNumber", hexutil.EncodeBig(number), false)
+	err := ipc.cli.Call(&block, "eth_getBlockByNumber", hexutil.EncodeUint64(number), false)
 	if err != nil {
 		return nil, err
 	}
@@ -48,25 +48,29 @@ func (ipc *IPCInterface) GetBlock(number *big.Int) (*models.Block, error) {
 	return &block, nil
 }
 
-func (ipc *IPCInterface) GetLastBlocks(count int64) ([]*models.Block, error) {
-	blockNumber, err := ipc.GetBlockNumber()
+func (ipc *IPCInterface) GetLastBlocks(count uint64) ([]*models.Block, error) {
+	last, err := ipc.GetBlockNumber()
 	if err != nil {
 		return nil, err
 	}
 
-	first := blockNumber.Int64() 
-	last := first - count
-	
-	blocks := make([]*models.Block, 0)
-	for i := first; i > last && i >= 0 ; i-- {
-		bl, err := ipc.GetBlock(big.NewInt(i))
+	first := last - count + 1
+	if first < 0 {
+		first = 0
+	}
+
+	log.Println(first, last, count)
+
+	blocks := make([]*models.Block, count)
+	for i := uint64(0); i < count; i++ {
+		bl, err := ipc.GetBlock(i + first)
 		if err != nil {
 			log.Println(err.Error())
 		} else {
-			blocks = append(blocks,bl)
+			blocks[i] = bl
 		}
 	}
-	
+
 	return blocks, nil
 }
 
