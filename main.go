66d4c351f1087642ad4c70822d1f79336593f021
Harry Kalogirou <harkal@gmail.com>
2018-01-25 17:39:04 +0200

    more command line work, flag for ipc

--- main.go
+++ main.go
@@ -5,12 +5,24 @@ import (
 	"ebakus_server/ipc"
 	"log"
 	"os"
+	"os/user"
+	"path/filepath"
 
 	"github.com/urfave/cli"
 )
 
-func pullNewBlocks() error {
-	ipc, err := ipc.NewIPCInterface("/Users/harkal/ebakus/ebakus.ipc")
+func expandHome(path string) string {
+	if path[:2] == "~/" {
+		usr, _ := user.Current()
+		dir := usr.HomeDir
+		path = filepath.Join(dir, path[2:])
+	}
+	return path
+}
+
+func pullNewBlocks(c *cli.Context) error {
+	ipcFile := expandHome(c.String("ipc"))
+	ipc, err := ipc.NewIPCInterface(ipcFile)
 	if err != nil {
 		log.Fatal("Failed to connect to ebakus", err)
 	}
@@ -62,14 +74,21 @@ func main() {
 	app.Copyright = "(c) 2018 Ebakus Team"
 	app.Usage = "Run in various modes depending on funcion mode"
 
+	genericFlags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "ipc",
+			Usage: "The ebakus node to connect to e.g. ./ebakus/ebakus.ipc",
+			Value: "~/ebakus/ebakus.ipc",
+		},
+	}
+
 	app.Commands = []cli.Command{
 		{
 			Name:    "fetchblocks",
 			Aliases: []string{"f"},
 			Usage:   "Fetch new blocks from ebakus node",
-			Action: func(c *cli.Context) error {
-				return pullNewBlocks()
-			},
+			Flags:   genericFlags,
+			Action:  pullNewBlocks,
 		},
 	}
 
