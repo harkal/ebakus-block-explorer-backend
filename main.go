bce7396fabcaf6a9b7a8894d7429827f488e470d
Harry Kalogirou <harkal@gmail.com>
2018-01-25 17:17:57 +0200

    command line parsing

--- main.go
+++ main.go
@@ -4,11 +4,12 @@ import (
 	"ebakus_server/db"
 	"ebakus_server/ipc"
 	"log"
+	"os"
+
+	"github.com/urfave/cli"
 )
 
-func main() {
-	log.Print("Starting...")
-
+func pullNewBlocks() error {
 	ipc, err := ipc.NewIPCInterface("/Users/harkal/ebakus/ebakus.ipc")
 	if err != nil {
 		log.Fatal("Failed to connect to ebakus", err)
@@ -39,4 +40,38 @@ func main() {
 		log.Fatal("Failed to insert blocks")
 	}
 
+	return err
+}
+
+func main() {
+	log.Print("Starting...")
+
+	app := cli.NewApp()
+	app.Name = "Ebakus Blockchain Explorer"
+	app.Version = "0.0.1"
+	app.Authors = []cli.Author{
+		cli.Author{
+			Name:  "Harry Kalogirou",
+			Email: "harkal@gmail.com",
+		},
+		cli.Author{
+			Name:  "Pantelis Giazitsis",
+			Email: "burn665@gmail.com",
+		},
+	}
+	app.Copyright = "(c) 2018 Ebakus Team"
+	app.Usage = "Run in various modes depending on funcion mode"
+
+	app.Commands = []cli.Command{
+		{
+			Name:    "fetchblocks",
+			Aliases: []string{"f"},
+			Usage:   "Fetch new blocks from ebakus node",
+			Action: func(c *cli.Context) error {
+				return pullNewBlocks()
+			},
+		},
+	}
+
+	app.Run(os.Args)
 }
