4e451f70a8d21bc8341c4e895b7fc07e217f860d
Harry Kalogirou <harkal@gmail.com>
2018-01-26 21:21:57 +0200

    block with marshaling

--- models/models.go
+++ models/models.go
@@ -6,32 +6,42 @@ import (
 	"github.com/ethereum/go-ethereum/core/types"
 )
 
+//go:generate gencodec -type Block -field-override blockMarshaling -out gen_block_json.go
+
 type Block struct {
-	Number           hexutil.Uint64
-	TimeStamp        hexutil.Uint64
-	Hash             common.Hash
-	ParentHash       common.Hash
-	StateRoot        common.Hash
-	TransactionsRoot common.Hash
-	ReceiptsRoot     common.Hash
-	Size             hexutil.Uint64
-	GasUsed          hexutil.Uint64
-	GasLimit         hexutil.Uint64
-	Transactions     []common.Hash
-	LogsBloom        types.Bloom
+	Number           hexutil.Uint64 `json:"number"`
+	TimeStamp        hexutil.Uint64 `json:"timestamp"`
+	Hash             common.Hash    `json:"hash"`
+	ParentHash       common.Hash    `json:"parent_hash"`
+	StateRoot        common.Hash    `json:"state_root"`
+	TransactionsRoot common.Hash    `json:"transactions_root"`
+	ReceiptsRoot     common.Hash    `json:"receipts_root"`
+	Size             hexutil.Uint64 `json:"size"`
+	GasUsed          hexutil.Uint64 `json:"gas_used"`
+	GasLimit         hexutil.Uint64 `json:"gas_limit"`
+	Transactions     []common.Hash  `json:"-"`
+	LogsBloom        types.Bloom    `json:"-"`
+}
+
+type blockMarshaling struct {
+	Number    uint64
+	TimeStamp uint64
+	Size      uint64
+	GasUsed   uint64
+	GasLimit  uint64
 }
 
 type Transaction struct {
-	Hash				common.Hash
-	Nonce				hexutil.Uint64
-	BlockHash 			common.Hash
-	BlockNumber 		hexutil.Uint64
-	TransactionIndex 	hexutil.Uint64
-	From				common.UnprefixedAddress
-	To					common.UnprefixedAddress
-	value 				hexutil.Uint64
-	GasPrice			hexutil.Uint64
-	Gas					hexutil.Uint64
+	Hash             common.Hash
+	Nonce            hexutil.Uint64
+	BlockHash        common.Hash
+	BlockNumber      hexutil.Uint64
+	TransactionIndex hexutil.Uint64
+	From             common.UnprefixedAddress
+	To               common.UnprefixedAddress
+	value            hexutil.Uint64
+	GasPrice         hexutil.Uint64
+	Gas              hexutil.Uint64
 	//TODO: Find type for input
-//	Input				TYPE?
+	//	Input				TYPE?
 }
