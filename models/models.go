f9a2e106a70e6b19f15de22f0fc6b3621f152968
George Koutsikos <ragecryx@gmail.com>
2018-02-24 23:28:09 +0200

    add json annotations & marshalling method for Transaction model

--- models/models.go
+++ models/models.go
@@ -27,8 +27,11 @@ type Block struct {
 
 type JSONBlock Block
 
+// MarshalJSON converts a Block to a byte array
+// that contains it's data in JSON format.
 func (b Block) MarshalJSON() ([]byte, error) {
-	type Block struct {
+	// Struct with only the fields we want in the final JSON?
+	var enc struct {
 		Number           uint64      `json:"number"`
 		TimeStamp        uint64      `json:"timestamp"`
 		Hash             common.Hash `json:"hash"`
@@ -40,7 +43,7 @@ func (b Block) MarshalJSON() ([]byte, error) {
 		GasUsed          uint64      `json:"gasUsed"`
 		GasLimit         uint64      `json:"gasLimit"`
 	}
-	var enc Block
+
 	enc.Number = uint64(b.Number)
 	enc.TimeStamp = uint64(b.TimeStamp)
 	enc.Hash = b.Hash
@@ -51,20 +54,30 @@ func (b Block) MarshalJSON() ([]byte, error) {
 	enc.Size = uint64(b.Size)
 	enc.GasUsed = uint64(b.GasUsed)
 	enc.GasLimit = uint64(b.GasLimit)
+
 	return json.Marshal(&enc)
 }
 
 type Transaction struct {
-	Hash             common.Hash
-	Nonce            hexutil.Uint64
-	BlockHash        common.Hash
-	BlockNumber      hexutil.Uint64
-	TransactionIndex hexutil.Uint64
-	From             common.UnprefixedAddress
-	To               common.UnprefixedAddress
-	value            hexutil.Uint64
-	GasPrice         hexutil.Uint64
-	Gas              hexutil.Uint64
+	Hash             common.Hash              `json:"hash"`
+	Nonce            hexutil.Uint64           `json:"nonce"`
+	BlockHash        common.Hash              `json:"blockHash"`
+	BlockNumber      hexutil.Uint64           `json:"blockNumber"`
+	TransactionIndex hexutil.Uint64           `json:"txIndex"`
+	From             common.UnprefixedAddress `json:"from"`
+	To               common.UnprefixedAddress `json:"to"`
+	Value            hexutil.Uint64           `json:"value"`
+	GasPrice         hexutil.Uint64           `json:"gasPrice"`
+	Gas              hexutil.Uint64           `json:"gas"`
 	//TODO: Find type for input
 	//	Input				TYPE?
+}
+
+// MarshalJSON converts a Transaction to a byte array
+// that contains it's data in JSON format.
+func (b Transaction) MarshalJSON() ([]byte, error) {
+	// Transactions don't need a subset of the fields
+	// currently, so we don't have to follow a similar
+	// procedure like the Block marshalling.
+	return json.Marshal(&b)
 }
