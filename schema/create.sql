3679b1ce9a21530e465a1ed4389c6981e37647a0
Chris Ziogas <ziogaschr@gmail.com>
2019-05-06 17:10:20 +0300

    Change db index key name

--- schema/create.sql
+++ schema/create.sql
@@ -41,4 +41,4 @@ CREATE TABLE transactions (
 CREATE INDEX txfrom_idx ON transactions USING btree (addr_from);
 CREATE INDEX txto_idx ON transactions USING btree (addr_to);
 CREATE INDEX txblock_hash_idx ON transactions USING btree (block_hash);
-CREATE INDEX timestamp_idx ON transactions USING btree (timestamp);
+CREATE INDEX txtimestamp_idx ON transactions USING btree (timestamp);
